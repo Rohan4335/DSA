abc2baed586e0f257969da17b8d4dc5dbc702492
Rohan4335 <139643497+Rohan4335@users.noreply.github.com>
2025-10-29 22:41:53 +0530

    Time: 5 ms (59.33%) | Memory: 45.6 MB (6.04%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -2,30 +2,22 @@ class Solution {
     public int romanToInt(String s) {
         int ans = 0;
         Map<Character, Integer> map = new HashMap<>();
-        for(int i=0; i<s.length(); i++){
-            char key1 = s.charAt(i);
-            if(key1 == 'I'){
-                map.put(key1, 1);
-            }
-            if(key1 == 'V'){
-                map.put(key1, 5);
-            }
-            if(key1 == 'X'){
-                map.put(key1, 10);
-            }
-            if(key1 == 'L'){
-                map.put(key1, 50);
-            }
-            if(key1 == 'C'){
-                map.put(key1, 100);
-            }
-            if(key1 == 'D'){
-                map.put(key1, 500);
-            }
-            if(key1 == 'M'){
-                map.put(key1, 1000);
-            }
-        }
+        
+                map.put('I', 1);
+            
+                map.put('V', 5);
+           
+                map.put('X', 10);
+           
+                map.put('L', 50);
+            
+                map.put('C', 100);
+            
+                map.put('D', 500);
+           
+                map.put('M', 1000);
+            
+        
         for(int i=0; i<s.length()-1; i++){
             if(map.get(s.charAt(i)) < map.get(s.charAt(i+1))){
                 ans = ans - map.get(s.charAt(i));
