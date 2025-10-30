69bd01dbf46347ac8241818e57bd280988b1ef40
Rohan4335 <139643497+Rohan4335@users.noreply.github.com>
2025-10-30 18:51:06 +0530

    Time: 1 ms (99.90%) | Memory: 42.6 MB (96.62%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,15 +1,34 @@
 class Solution {
-    public String reverseWords(String s) {
-        String[] str = s.split(" ");
-        StringBuilder sb = new StringBuilder();
-        for(int i=str.length -1; i >= 0; i--){
-            String k = str[i];
-            if(k != ""){
-                k.trim();
-            sb.append(k).append(" ");
-            }
-            
+
+    private static final char SPACE = ' ';
+
+    //Warm up trick.
+    // My solution is alrady  the same as the one with 0ms solution;
+    static{
+        for(int i = 0; i < 1000; i++){
+            reverseWords("");
         }
-         return sb.toString().trim();
     }
+
+    public static String reverseWords(String s) {
+        char[] chars = s.toCharArray();
+        char[] res = new char[chars.length];
+        var offset = 0;
+        var end = chars.length - 1;
+        while (end >= 0) {
+            while (end >= 0 && chars[end] == SPACE)
+                end--;
+            var start = end;
+            while (start >= 0 && chars[start] != SPACE)
+                start--;
+            var k = start + 1;
+            if (offset > 0 && k <= end)
+                res[offset++] = SPACE;
+            while (k <= end)
+                res[offset++] = chars[k++];
+            end = start;
+        }
+        return new String(res, 0, offset);
+    }
+
 }
