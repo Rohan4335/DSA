0d99b71209506d6d911979684a23ff9d4e45291f
Rohan4335 <139643497+Rohan4335@users.noreply.github.com>
2025-11-02 18:43:01 +0530

    Time: 6 ms (49.12%) | Memory: 44.2 MB (5.04%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.java
+++ 299-bulls-and-cows/bulls-and-cows.java
@@ -2,31 +2,24 @@ class Solution {
     public String getHint(String secret, String guess) {
         int bulls = 0;
         int cows = 0;
-        StringBuilder secret1 = new StringBuilder(secret);
-        StringBuilder guess1 = new StringBuilder(guess);
-        for(int i =0; i<secret1.length(); i++){
-            if(secret1.charAt(i) == guess1.charAt(i)){
+        Map<Character,Integer> sec = new HashMap<>();
+        Map<Character,Integer> gue = new HashMap<>();// Peh
+        for(int i= 0; i<secret.length(); i++){
+            char s = secret.charAt(i);
+            char g = guess.charAt(i);
+            if(s==g){
                 bulls++;
             }
-        }
-        for(int i= 0; i<secret1.length(); i++){
-            char ch = secret1.charAt(i);
-            if(secret1.charAt(i) != guess1.charAt(i)){
-                for(int j=0; j<guess1.length(); j++){
-                    if(secret1.charAt(j) != guess1.charAt(j)){
-                    if(ch == guess1.charAt(j)){
-                        cows++;
-                        guess1.setCharAt(j, ' ');
-                        break;
-                    }
-                    }
-                }
+            else{
+                sec.put(s, sec.getOrDefault(s,0)+1);
+                gue.put(g, gue.getOrDefault(g,0)+1);
             }
         }
-        Integer.toString(bulls);
-        Integer.toString(cows);
-        String ans = bulls + "" + "A" + cows + "B";
-        return ans;
-
+        for(char c : sec.keySet()){
+            if(gue.containsKey(c)){
+                cows += Math.min(sec.get(c),gue.get(c));
+            }
+        }
+        return bulls + "A"+cows+"B";
     }
 }
