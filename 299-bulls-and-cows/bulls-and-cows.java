a7625ba67c3a99bdc6f5deecca20f27dced43c0e
Rohan4335 <139643497+Rohan4335@users.noreply.github.com>
2025-11-02 19:00:49 +0530

    Time: 3 ms (92.39%) | Memory: 43.5 MB (6.35%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.java
+++ 299-bulls-and-cows/bulls-and-cows.java
@@ -2,24 +2,19 @@ class Solution {
     public String getHint(String secret, String guess) {
         int bulls = 0;
         int cows = 0;
-        Map<Character,Integer> sec = new HashMap<>();
-        Map<Character,Integer> gue = new HashMap<>();// Peh
-        for(int i= 0; i<secret.length(); i++){
+        int[] freq = new int[10];
+        for(int i=0; i<secret.length(); i++){
             char s = secret.charAt(i);
-            char g = guess.charAt(i);
-            if(s==g){
-                bulls++;
-            }
-            else{
-                sec.put(s, sec.getOrDefault(s,0)+1);
-                gue.put(g, gue.getOrDefault(g,0)+1);
+            freq[s-'0']++;
+        }
+        for(int i=0; i<guess.length(); i++){
+            if(secret.charAt(i) == guess.charAt(i)) bulls++;
+             if(freq[guess.charAt(i)-'0'] != 0){
+                cows++;
+                freq[guess.charAt(i)-'0']--;
             }
         }
-        for(char c : sec.keySet()){
-            if(gue.containsKey(c)){
-                cows += Math.min(sec.get(c),gue.get(c));
-            }
-        }
+        cows = cows - bulls;
         return bulls + "A"+cows+"B";
     }
 }
