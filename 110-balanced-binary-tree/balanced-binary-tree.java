3359808a3fe645d156cd8b2186ffec78761f77b2
Rohan4335 <139643497+Rohan4335@users.noreply.github.com>
2025-12-03 18:52:51 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (35.62%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -0,0 +1,15 @@
+class Solution {
+    boolean ans = true;
+    public boolean isBalanced(TreeNode root) {
+        helper(root);
+        return ans;
+    }
+
+    public int helper(TreeNode root){
+        if(root == null) return 0;
+        int left = helper(root.left);
+        int right = helper(root.right);
+        if(Math.abs(left-right)>1) ans = false;
+        return Math.max(left, right)+1;
+    }
+}
