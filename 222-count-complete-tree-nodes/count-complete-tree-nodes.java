f330ac828fb3494d83da24ad31896ee716357800
Rohan4335 <139643497+Rohan4335@users.noreply.github.com>
2025-12-18 12:12:20 +0530

    Time: 0 ms (100.00%) | Memory: 49.6 MB (15.27%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -14,12 +14,22 @@
  * }
  */
 class Solution {
-    int count = 0;
     public int countNodes(TreeNode root) {
-        if(root == null) return 0;
-        count = count +1;
-        countNodes(root.left);
-        countNodes(root.right);
-        return count;
+        int leftDepth = leftDepth(root);
+        int rightDepth = rightDepth(root);
+        if(leftDepth == rightDepth){
+            return (int)Math.pow(2, leftDepth)-1;
+        }else{
+            return 1 + countNodes(root.left) + countNodes(root.right);
+        }
+    }
+    public int leftDepth(TreeNode root){
+        if(root == null)return 0;
+        return 1 + leftDepth(root.left);
+    }
+
+    public int rightDepth(TreeNode root){
+        if(root == null)return 0;
+        return 1 + rightDepth(root.right);
     }
 }
