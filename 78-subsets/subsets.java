82582c5e04f55ca27ebf66ea879b0e93a99c9e39
Rohan4335 <139643497+Rohan4335@users.noreply.github.com>
2026-01-17 08:34:01 +0530

    Time: 1 ms (87.92%) | Memory: 44.3 MB (44.37%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,15 +1,16 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans= new ArrayList<>();
-        backtrack(0, nums, new ArrayList<>(), ans);
-        return ans;
-    }
-    public void backtrack(int idx, int[]nums, List<Integer> cur, List<List<Integer>> ans){
-        ans.add(new ArrayList<>(cur));
-        for(int i=idx; i<nums.length;i++){
-            cur.add(nums[i]);
-            backtrack(i+1, nums, cur, ans);
-            cur.remove(cur.size()-1);
+        List<List<Integer>> ans = new ArrayList<>();
+        int subsets = 1<<nums.length;
+        for(int i = 0; i<subsets; i++){
+            List<Integer> list = new ArrayList<>();
+            for(int j = 0; j<nums.length;j++){
+                if((i & (1<<j)) != 0){
+                    list.add(nums[j]);
+                }
+            }
+            ans.add(list);
         }
+        return ans;
     }
 }
