8fbd6f5387d5efb60bef84eb59bc175ec6abf1ce
Rohan4335 <139643497+Rohan4335@users.noreply.github.com>
2026-01-22 12:36:33 +0530

    Time: 16 ms (81.39%) | Memory: 46.6 MB (43.69%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,60 +1,16 @@
 class Solution {
-    static final int mod = 1000000007;
-
     public long subArrayRanges(int[] nums) {
-        int n = nums.length; 
-        Stack<Integer> st = new Stack<>();
-        int[] leftarr = new int[n];
-        int[] rightarr = new int[n];
-        //leftmin
+        int n = nums.length;
+        long sum = 0;
         for(int i=0; i<n; i++){
-            while(!st.isEmpty() && nums[st.peek()] > nums[i]){
-                st.pop();
+            int min = nums[i];
+            int max = nums[i];
+            for(int j = i; j<n; j++){
+                min = Math.min(min, nums[j]);
+                max = Math.max(max, nums[j]);
+                sum += max-min;
             }
-            leftarr[i] = st.isEmpty()? -1: st.peek();
-            st.push(i);
         }
-        st.clear();
-        //rightmin
-        for(int i=n-1; i>=0; i--){
-            while(!st.isEmpty() && nums[st.peek()] >= nums[i]){
-                st.pop();
-            }
-            rightarr[i] = st.isEmpty()? n: st.peek();
-            st.push(i);
-        }
-        st.clear();
-        int[] leftmax = new int[n];
-        int[] rightmax = new int[n];
-        //leftmax
-        for(int i=0; i<n; i++){
-            while(!st.isEmpty() && nums[st.peek()] <= nums[i]){
-                st.pop();
-            }
-            leftmax[i] = st.isEmpty()? -1: st.peek();
-            st.push(i);
-        }
-        st.clear();
-        //leftmax
-        for(int i=n-1; i>=0; i--){
-            while(!st.isEmpty() && nums[st.peek()] < nums[i]){
-                st.pop();
-            }
-            rightmax[i] = st.isEmpty()? n: st.peek();
-            st.push(i);
-        }
-        long minsum = 0;
-        for(int i = 0; i<n; i++){
-            long leftLen = i - leftarr[i];
-            long rightLen = rightarr[i] - i;
-            minsum += leftLen*rightLen*nums[i];
-        }
-        long maxsum = 0;
-        for(int i = 0; i<n; i++){
-            long leftLen = i - leftmax[i];
-            long rightLen = rightmax[i] - i;
-            maxsum += leftLen*rightLen*nums[i];
-        }
-        return maxsum-minsum;
+        return sum;
     }
 }
