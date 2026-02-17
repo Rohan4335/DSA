75457e0c71482a99babf76ef4be926148701a823
Rohan4335 <139643497+Rohan4335@users.noreply.github.com>
2026-02-17 10:02:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (62.34%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,11 +1,21 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int row = 0;
-        int col = matrix[0].length-1;
-        while(row < matrix.length && col >= 0 ){
-            if(matrix[row][col] == target) return true;
-            else if(target > matrix[row][col]) row++;
-            else col--;
+        for(int[] row: matrix){
+            boolean flag = helper(row, target);
+            if(flag){
+                return true;
+            }
+        }
+            return false;
+    }
+    public boolean helper(int[] row, int target){
+        int low = 0;
+        int high = row.length-1;
+        while(low <= high){
+            int mid = low + (high- low)/2;
+            if(row[mid] == target) return true;
+            else if(row[mid] > target) high = mid-1;
+            else low = mid + 1;
         }
         return false;
     }
