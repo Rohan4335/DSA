a2b20be9c148788b26e3b3ecb62f798cd2c82991
Rohan4335 <139643497+Rohan4335@users.noreply.github.com>
2026-04-03 11:58:29 +0530

    Time: 12 ms (99.53%) | Memory: 47 MB (99.43%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -3,30 +3,29 @@ class Solution {
         int n = hand.length;
         if(n % groupSize != 0) return false;
         Arrays.sort(hand);
-        for(int i=0; i<n; i++){
-            if(hand[i] >= 0){
-                if(!findsuccessors(hand, groupSize, i, n)){
-                    return false;
+        for(int i = 0; i<n; i++){
+            if(hand[i] >=0){
+                if(!isPossible(hand, groupSize, i, n)){
+                    return false; 
                 }
             }
         }
         return true;
     }
-
-    public boolean findsuccessors(int[] hand, int groupSize, int i, int n){
+    public boolean isPossible(int[] hand, int groupSize, int i , int n){
         int f = hand[i] + 1;
         hand[i] = -1;
-        int count = 1;
-        i+=1;
-        while(i < n && count < groupSize){
+        int cnt = 1;
+        i += 1;
+        while(i<n && cnt < groupSize){
             if(hand[i] == f){
                 f = hand[i] + 1;
                 hand[i] = -1;
-                count++;
+                cnt++;
             }
             i++;
         }
-        if(count != groupSize) return false;
+        if(cnt != groupSize) return false;
         else return true;
     }
 }
